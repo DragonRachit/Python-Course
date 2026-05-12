92b284a928a46de57a0296f5ecbb9b1e7eec6b6b
DragonRchit <rachitbhandari774@gmail.com>
2026-05-12 13:19:14 +0545

    Update csv code

--- Python class/csv_dict_reader.py
+++ Python class/csv_dict_reader.py
@@ -1,8 +1,8 @@
 import csv
 
-with open("practice.csv", 'w') as f:
+with open("practice.csv", 'w', newline="") as f:
     head=["Name", "Age"]
     writer=csv.DictWriter(f, fieldnames=head)
     writer.writeheader()
-    
-
+    writer.writerow({"Name": "Ram", "Age": 12})
+    writer.writerow({"Name": "Shyam", "Age": 12})
